5c5420b34d5d11de35e8f60ca68ced2d1bf88c7d
Armin Reichert <armin.reichert@web.de>
2023-05-22 13:41:04 +0200

    Improved tinylog mockup

--- pacman-core/src/main/java/org/tinylog/Logger.java
+++ pacman-core/src/main/java/org/tinylog/Logger.java
@@ -1,21 +1,28 @@
 package org.tinylog;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author Bruno Salmon
+ * @author Armin Reichert
  */
 public class Logger {
 
     private static void println(String message, Object... args) {
-        System.out.print(message + ": ");
-        for (Object arg : args) {
-            System.out.print(" " + arg);
+        message = message.replaceAll("\\{[^}]*\\}", "__");
+        for (int i = 0; i < args.length; ++i) {
+            message = message.replaceFirst("__", String.valueOf(args[i]));
         }
-        System.out.println();
+        var dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss SSS");
+        var prefix = LocalDateTime.now().format(dateFormat) + ": ";
+        System.out.println(prefix + message);
     }
 
     public static void trace(String message, Object... parameters) {
-        println(message, parameters);
-    }
+        println(message, parameters); }
 
     public static void info(String message, Object... parameters) {
         println(message, parameters);
