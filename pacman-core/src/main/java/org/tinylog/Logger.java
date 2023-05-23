995a80e7ecb4436591e2c2dc9b96e6b78d2d1af3
Armin Reichert <armin.reichert@web.de>
2023-05-23 22:27:19 +0200

    Fixed issue in tinylog mockup.

--- pacman-core/src/main/java/org/tinylog/Logger.java
+++ pacman-core/src/main/java/org/tinylog/Logger.java
@@ -4,6 +4,8 @@ import dev.webfx.platform.console.Console;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.function.Function;
+import java.util.regex.*;
 
 /**
  * @author Bruno Salmon
@@ -11,14 +13,35 @@ import java.time.format.DateTimeFormatter;
  */
 public class Logger {
 
+/* GWT does not support Java regular expression stuff. It works fine in the VM version.
     private static void println(String message, Object... args) {
-        message = message.replaceAll("\\{[^}]*\\}", "__");
-        for (var arg : args) {
-            message = message.replaceFirst("__", String.valueOf(arg));
-        }
         var dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss SSS");
         var prefix = LocalDateTime.now().format(dateFormat) + ": ";
+        var pattern = Pattern.compile("\\{.*?\\}");
+        int i = 0;
+        do {
+            var matcher = pattern.matcher(message);
+            if (!matcher.find() || i == args.length) {
+                break;
+            }
+            message = matcher.replaceFirst(Matcher.quoteReplacement(String.valueOf(args[i])));
+            i += 1;
+        } while (true);
         Console.log(prefix + message);
+    }
+*/
+    private static void println(String message, Object... args) {
+        var dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss SSS");
+        var prefix = LocalDateTime.now().format(dateFormat) + ": ";
+        var argsText = new StringBuilder(" (");
+        for (int i = 0; i < args.length; ++i) {
+            argsText.append(args[i]);
+            if (i < args.length - 1) {
+                argsText.append(", ");
+            }
+        }
+        argsText.append(")");
+        Console.log(prefix + message + argsText);
     }
 
     public static void trace(String message, Object... parameters) {
